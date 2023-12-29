2438e623558ab31f30f6f60d3515858a313d2a5e
劉軒宏 <3b032110@gm.student.ncut.edu.tw>
2023-12-30 06:14:41 +0800

    刪除 2-7-4 賣家可查看商品列表 不必要程式碼

--- resources/views/sellers/products/index.blade.php
+++ resources/views/sellers/products/index.blade.php
@@ -31,11 +31,6 @@
                                     <td>{{$data->amount}}</td>
                                     <td>{{$data->updated_at}}</td>
                                 </tr>
-                                    {{-- <tr>
-                                        <th scope="row">{{$data->id}}</th>
-                                        <th>{{$data->amount}}</th>
-                                        <th>{{$data->updated_at}}</th>
-                                    </tr> --}}
                             @endforeach
                         </tbody>
                     </table>
@@ -44,28 +39,3 @@
         </div>
     </div>
 </x-seller-layout>
-
-
-
-
-{{-- <!-- 這是一個簡單的 Blade View 範例，你可以根據你的需求進行調整 -->
-<h2>賣家商品列表</h2>
-
-<table>
-    <thead>
-        <tr>
-            <th>商品名稱</th>
-            <th>價格</th>
-            <!-- 其他相關欄位 -->
-        </tr>
-    </thead>
-    <tbody>
-        @foreach($products as $product)
-            <tr>
-                <td>{{ $product->name }}</td>
-                <td>{{ $product->price }}</td>
-                <!-- 其他相關欄位 -->
-            </tr>
-        @endforeach
-    </tbody>
-</table> --}}
