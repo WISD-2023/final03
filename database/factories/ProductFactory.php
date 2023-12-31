16234376e79631042828930adf927a90f34fb426
吳宇翰 <3b032117@gm.student.ncut.edu.tw>
2024-01-01 04:53:32 +0800

    調低模擬資料的商品金額

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -23,7 +23,7 @@ class ProductFactory extends Factory
             'photo_url' => 'https://cdn.discordapp.com/attachments/631735793961926656/1184145570542125198/image.png',
             'amount' => mt_rand(2000,15000),
             'format' => $arr[mt_rand(0,5)],
-            'price' => mt_rand(5,2000),
+            'price' => mt_rand(5,200),
             'is_display' => rand(0,1),
         ];
     }
