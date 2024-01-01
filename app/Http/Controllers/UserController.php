9bb9f1973e0bd30d72ae88bc921de5c8895bc72f
吳宇翰 <3b032117@gm.student.ncut.edu.tw>
2024-01-01 15:22:56 +0800

    修正申請為賣家功能的錯誤

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -69,25 +69,14 @@ class UserController extends Controller
         // 可以在這裡加入相關的邏輯，例如檢查是否已經是賣家等
         // dd($user);
 
-
         // $user id 插入到 seller table 的 user_id 欄位 其他的則為 $data 內的資料
         $user->seller()->create([
             'user_id' => $user,
-            'secret_key' => "A8bckaUs0cQ8WpYqwZXkOj03lWFUQrxw",
-            'secret_iv' => "CzEEgu89GoaKZPLP",
+            'merchant' => "MS151149294",
+            'secret_key' => "p8hfQaNqqtD1e8hmo8uE0ShNPqshLUYq",
+            'secret_iv' => "CJRH9d4TxO59bO0P",
         ]);
 
-        //return Redirect::route('users.profile.edit')->with('status', 'users-updated');
         return redirect()->route('users.profile.edit')->with('status', 'users-updated');
-        // 更新資料庫
-        // $user->update(['is_seller' => true]);
-
-        // 顯示成功訊息，這裡使用 Laravel 的 with 方法，需要在相應的 view 中顯示
-        // return "申請成功 ".$user."";
-        // return view('users.seller');
-        // return view('users.seller', [
-        //     'user' => $user->user(),
-        // ]);
-        // return redirect()->route('users.seller')->with('success', '申請成功');
     }
 }
