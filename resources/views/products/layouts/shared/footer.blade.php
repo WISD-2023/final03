d268b8e5c1c66d2dfee012a28cec2816129ad2ab
劉軒宏 <3b032110@gm.student.ncut.edu.tw>
2024-01-03 10:08:47 +0800

    新增底頁源專案連結

--- resources/views/products/layouts/shared/footer.blade.php
+++ resources/views/products/layouts/shared/footer.blade.php
@@ -1,6 +1,6 @@
 <!-- Footer-->
 <footer class="py-5 bg-dark">
-    <div class="container"><p class="m-0 text-center text-white">Copyright &copy; NCUT WISD-03 2023</p></div>
+    <div class="container"><p class="m-0 text-center text-white">Copyright &copy; <a class="m-0 text-center text-white" href="https://github.com/WISD-2023/final03" target="_blank" rel="noopener noreferrer">NCUT WISD-03 2023</a></p></div>
 </footer>
 <!-- Bootstrap core JS-->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
